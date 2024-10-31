8ec12b3aa507c66e90cfcca3d08e369a9152a9ac
Gerben Aaltink <gerbenaaltink@gmail.com>
2024-10-31 20:54:18 +0100

    Update

--- src/stogram_client/rlib.py
+++ src/stogram_client/rlib.py
@@ -1,8 +1,26 @@
 import importlib.resources as pkg_resources
+import json 
+import time 
 
 def get_rlib_path():
     binary_path = pkg_resources.files('stogram_client.binaries').joinpath('librlib.so')
     return binary_path
+
+def generate_huge_json():
+    rows = []
+    str_value = [str(x) for x in range(100)]
+    for x in range(10000):
+        data={}
+        data["property_{}_str".format(x)] = str_value
+        data["property_{}_int".format(x)] = x * 2
+        data["property_{}_dec".format(x)] = 0.0 + x 
+        data["property_{}_null".format(x)] = None
+        data["property_{}_bool_true".format(x)] = True
+        data["property_{}_bool_false".format(x)] = False
+        data["property_{}_array"] = [str_value,x*3,0.0+x+1,None,True,False]
+        rows.append(data)
+    obj = dict(rows=rows)
+    return json.dumps(obj)
 
 import ctypes as ct 
 
@@ -56,6 +74,20 @@ class RlibTestCase(unittest.TestCase):
         self.assertEqual(json_length("{}{}["),2)
 
 def test():
+    
+
+    json_ = generate_huge_json()
+    time_start = time.time()
+    print(json_length(json_))
+    duration_python = time.time() - time_start 
+    time_start = time.time()
+    json.loads(json_)
+    duration_rliza = time.time() - time_start 
+    with open("large.json","w") as f:
+        f.write(json_)
+    print("Speed python:",duration_python,"Speed rliza:",duration_rliza)
+
+
     suite = unittest.TestLoader().loadTestsFromTestCase(RlibTestCase)
     runner = unittest.TextTestRunner()
     runner.run(suite) 
